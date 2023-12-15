b1c1e481168c67bb7d73981591c9ced36d2438ee
Za Matheson <zamatheson@Zas-MacBook-Air.local>
2023-12-14 21:52:19 -0600

    added starter code to some handlebars and troubleshooted

--- models/comment.js
+++ models/comment.js
@@ -31,7 +31,7 @@ Comment.init (
             type: DataTypes.INTEGER,
             allowNull: false,
             references: {
-                model: 'post',
+                model: 'blogPost',
                 key: 'id'
             },
         },
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const path = require("path");
 require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
 const hbs = exphbs.create({ helpers });
 
-const sequelize = require("./config/connection");
+const session = require("express-session");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sequelize = require("./config/connection");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
